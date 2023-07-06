5c43ae135359d3a7f8721eb329202d09291fb7f7
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2023-07-06 22:03:39 +0100

    Create wave

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 import NavBar from '@/components/navBar'
+import Wave from '@/components/wave'
 
 export default function Home() {
   return (
     <main className="container mx-auto bg-[var(--oceanColour)] mt-4 rounded-3xl border-4 border-[var(--cream)] overflow-hidden">
       <NavBar/>
-      <div className=" h-screen w-full bathboy bg-[url('https://raw.githubusercontent.com/reannab16/miss-soapy-suds-website/main/public/bathman3.2.png')] bg-contain bg-no-repeat bg-center"></div>
+      <div className=" h-[700px] w-full bathboy bg-[url('https://raw.githubusercontent.com/reannab16/miss-soapy-suds-website/main/public/bathmantangl.png')] bg-contain bg-no-repeat bg-top flex flex-col items-center justify-end">
+        <Wave/>
+      </div>
       
     </main>
   )
---
+++ src/components/wave.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+export default function Wave() {
+  return (
+    <svg width="100%" height="200px" fill="none">
+      <path
+        fill="var(--cream)"
+        d="
+        M0 77 
+            C 473,283
+              822,-40
+              1720,100 
+          
+          V 359 
+          H 0 
+          V 67
+          Z">
+        <animate 
+          repeatCount="indefinite" 
+          fill="url(#grad1)" 
+          attributeName="d" 
+          dur="20s"
+          attributeType="XML"
+          values="
+            M0 77 
+            C 473,283
+              822,-40
+              1720,100 
+            
+            V 359 
+            H 0 
+            V 67 
+            Z; 
+
+            M0 107 
+            C 473,-70
+              1122,300
+              1320,75
+            
+            V 359 
+            H 0 
+            V 67 
+            Z; 
+
+            
+
+            M0 77 
+            C 473,283
+              822,-40
+              1720,100
+            
+            V 359 
+            H 0 
+            V 67 
+            Z
+            ">
+        </animate>
+      </path>
+      
+      
+
+    </svg>
+  );
+}
