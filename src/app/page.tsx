ad6a9e5378401f3d34f6e0b40ea0cc1c2bf8c0a7
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2023-07-06 22:39:12 +0100

    Update bathmantext image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="container mx-auto bg-[var(--oceanColour)] mt-4 rounded-3xl border-4 border-[var(--cream)] overflow-hidden">
       <NavBar/>
-      <div className=" h-[700px] w-full bathboy bg-[url('https://raw.githubusercontent.com/reannab16/miss-soapy-suds-website/main/public/bathmantangl.png')] bg-contain bg-no-repeat bg-top flex flex-col items-center justify-end">
+      <div className=" h-[700px] w-full bg-[url('https://raw.githubusercontent.com/reannab16/miss-soapy-suds-website/main/public/bathmantext.png')] bg-contain bg-no-repeat bg-top flex flex-col items-center justify-end">
         <Wave/>
       </div>
       
