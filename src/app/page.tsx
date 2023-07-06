0379a6d1ad4b0a403a0479fcc846021f84047e4c
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2023-07-06 17:06:53 +0100

    Upload images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="container mx-auto bg-[var(--oceanColour)] mt-4 rounded-3xl border-4 border-[var(--cream)] overflow-hidden">
       <NavBar/>
-      <div className="h-screen w-screen bathboy"></div>
+      <div className="h-screen w-screen bg-[url(https://raw.githubusercontent.com/reannab16/miss-soapy-suds/main/public/bathman.png)]"></div>
       
     </main>
   )
